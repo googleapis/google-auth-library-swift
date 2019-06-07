f7371c8b4f3ce41a1b63b87c6fb04c53db850484
Antoine Barrault <barrault01@gmail.com>
2019-06-06 21:32:58 -0300

    update BitInt version to 4.0.0

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
     .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
-    .package(url: "https://github.com/attaswift/BigInt", from: "3.1.0"),
+    .package(url: "https://github.com/attaswift/BigInt", from: "4.0.0"),
   ],
   targets: [
     .target(name: "OAuth1",
