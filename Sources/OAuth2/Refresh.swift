3564c4a58ae59c1b9909214b8e5d90b556f918fe
David Cowden <david@uno.app>
2021-12-09 14:21:28 -0700

    build: fix linux missing imports

--- Sources/OAuth2/Refresh.swift
+++ Sources/OAuth2/Refresh.swift
@@ -13,6 +13,9 @@
 // limitations under the License.
 
 import Foundation
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
 
 protocol RefreshExchangeInfo {
   var accessTokenURL: String { get }
